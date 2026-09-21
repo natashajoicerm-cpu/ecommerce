f0f9de6aadd7dc5ca8a5340cab1afa9295d43f17
natashajoicerm-cpu <natashajoicerm@gmail.com>
2026-09-22 00:50:12 +0530

    login

--- core/urls.py
+++ core/urls.py
@@ -19,8 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users',include('users.urls')),
-    path('api/orders',include('orders.urls')),
-    path('api/products',include('products.urls')),
-    path('api/cart',include('cart.urls'))
+    path('api/users/',include('users.urls')),
+    path('api/orders/',include('orders.urls')),
+    path('api/products/',include('products.urls')),
+    path('api/cart/',include('cart.urls'))
 ]

c4ae83013ed5cac171f2ec93bbfc7865f62f017d
natashajoicerm-cpu <natashajoicerm@gmail.com>
2026-09-22 22:06:29 +0530

    register

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
 
 # Generate JWT Token
@@ -15,6 +16,7 @@ def get_tokens_for_user(user):
 
 # Register API
 class RegisterView(APIView):
+    permission_classes= [AllowAny]
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
 
@@ -35,6 +37,7 @@ class RegisterView(APIView):
 
 # Login API
 class LoginView(APIView):
+    permission_classes= [AllowAny]
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
 
@@ -51,3 +54,4 @@ class LoginView(APIView):
             })
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
